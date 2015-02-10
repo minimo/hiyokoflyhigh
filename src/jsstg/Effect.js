3aa1eff3e28b4b54c1f98ae77fb978c2ff201b16
minimo <minimo.mobile@gmail.com>
2015-02-10 15:29:15 +0900

    敵出現ワーニング表示

--- src/jsstg/Effect.js
+++ src/jsstg/Effect.js
@@ -27,4 +27,33 @@ tm.define("jsstg.Effect.Explode", {
     },
 });
 
+//敵出現ワーニング
+tm.define("jsstg.Effect.Warning", {
+    superClass: "tm.display.OutlineLabel",
+    layer: LAYER_EFFECT_UPPER,
+
+    image: null,
+    isEffect: true,
+    isUpper: true,
+
+    init: function(dir, y) {
+        y = y || SC_H*0.5;
+        var x = SC_W*0.15;
+        var text = "＜ WARNING!!";
+        if (dir == 1) {
+            x = SC_W*0.85;
+            text = "WARNING!! ＞";
+        }
+        this.superInit(text, 40);
+
+        this.setPosition(x, y);
+        this.time = 1;
+    },
+    update: function() {
+        if (this.time % 10 == 0) this.visible = !this.visible;
+        if (this.time > 180) this.remove();
+        this.time++;
+    },
+});
+
 })();
