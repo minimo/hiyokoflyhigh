9ecc033eaee7134e65805a483ab14ace8fd1483c
minimo <minimo.mobile@gmail.com>
2015-02-20 15:15:12 +0900

    敵追加

--- src/jsstg/EnemyData.js
+++ src/jsstg/EnemyData.js
@@ -203,6 +203,68 @@ tm.define("jsstg.enemyData.mecha1", {
 });
 jsstg.enemyData["mecha1"] = jsstg.enemyData.mecha1;
 
+//メカひよこ２
+tm.define("jsstg.enemyData.mecha2", {
+    superClass: "jsstg.Enemy",
+
+    //使用弾幕パターン
+    bulletPattern: "cube1",
+
+    //当り判定サイズ
+    width:  16,
+    height: 16,
+
+    //耐久力
+    def: 2,
+
+    //得点
+    point: 100,
+
+    //表示レイヤー番号
+    layer: LAYER_OBJECT,
+
+    init: function(x, y, param) {
+        this.superInit(x, y, param);
+
+        this.sprite = tm.display.AnimationSprite(jsstg.SpriteSheet.mecha, 32, 32)
+            .addChildTo(this)
+            .setScale(2)
+            .gotoAndPlay("fly");
+    },
+
+    algorithm: function() {
+        this.x -= 2;
+    },
+
+    dead: function() {
+        this.isCollision = false;
+        this.isDead = true;
+        this.remove();
+
+        var that = this;
+        var sp = tm.display.Sprite("mecha", 32, 32)
+            .addChildTo(this.parentScene)
+            .setPosition(this.x, this.y)
+            .setScale(2)
+            .setFrameIndex(4);
+        sp.vy = -10;
+        sp.layer = LAYER_EFFECT_LOWER;
+        sp.update = function() {
+            this.rotation+=10;
+            this.x += 1;
+            this.y += this.vy;
+            this.vy += 0.98*0.5;
+            if (this.y > SC_H*0.9) {
+                this.remove();
+                jsstg.Effect.Explode2()
+                    .addChildTo(that.parentScene)
+                    .setPosition(this.x, this.y-16);
+            }
+        };
+    },
+});
+jsstg.enemyData["mecha2"] = jsstg.enemyData.mecha2;
+
 //メカひよこ（地上）
 tm.define("jsstg.enemyData.mecha$", {
     superClass: "jsstg.Enemy",
--- src/jsstg/EnemyUnit.js
+++ src/jsstg/EnemyUnit.js
@@ -69,6 +69,26 @@ jsstg.enemyUnit = [
         { "name": "waru2",   "x":SC_W*1.1, "y":SC_H*0.70, "delay":120 },
     ]
 },
+{
+    name: "mecha2-1",
+    rank: 0,
+    time: 300,
+    warn: { "y":SC_H*0.50, "reverse":0 },
+    enemies: [
+        { "name": "mecha2",   "x":SC_W*1.1, "y":SC_H*0.50 },
+    ]
+},
+{
+    name: "mecha2-2",
+    rank: 0,
+    time: 300,
+    warn: { "y":SC_H*0.50, "reverse":0 },
+    enemies: [
+        { "name": "mecha2",   "x":SC_W*1.3, "y":SC_H*0.40 },
+        { "name": "mecha2",   "x":SC_W*1.1, "y":SC_H*0.50 },
+        { "name": "mecha2",   "x":SC_W*1.3, "y":SC_H*0.60 },
+    ]
+},
 
 ]
 })();
