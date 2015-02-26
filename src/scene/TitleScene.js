0b679fca98f43bb9684d707c326d7ea47dcf2634
minimo <minimo.mobile@gmail.com>
2015-02-26 20:18:11 +0900

    タイトル更新

--- src/scene/TitleScene.js
+++ src/scene/TitleScene.js
@@ -13,24 +13,37 @@ tm.define("jsstg.TitleScene", {
         this.superInit();
         app.background = "rgba(0, 0, 0, 1.0)";
 
-        var param = {
+        var param1 = {
             fontFamily: "azukiP",
             align: "center",
             baseline: "middle",
             outlineWidth: 1
         };
+        var param2 = {
+            fontFamily: "azukiP",
+            align: "center",
+            baseline: "middle",
+            outlineWidth: 1,
+            fillStyle: "rgb(100,255,100)",
+        };
         this.t1 = tm.display.OutlineLabel("ひよこは", 60)
             .addChildTo(this)
-            .setParam(param)
-            .setPosition(SC_W*0.3, SC_H*0.2);
+            .setParam(param1)
+            .setPosition(SC_W*0.3, SC_H*0.3);
         this.t2 = tm.display.OutlineLabel("意外と高く", 60)
             .addChildTo(this)
-            .setParam(param)
-            .setPosition(SC_W*0.7, SC_H*0.2);
+            .setParam(param2)
+            .setPosition(SC_W*0.7, SC_H*0.3)
+            .setRotation(30);
         this.t3 = tm.display.OutlineLabel("空を飛ぶ", 60)
             .addChildTo(this)
-            .setParam(param)
-            .setPosition(SC_W*0.4, SC_H*0.3);
+            .setParam(param1)
+            .setPosition(SC_W*0.5, SC_H*0.5);
+
+        this.t3 = tm.display.OutlineLabel("Touch to Start", 30)
+            .addChildTo(this)
+            .setParam(param1)
+            .setPosition(SC_W*0.5, SC_H*0.7);
 
         this.time = 0;
     },
